64dd43bb8228db515aa9934169391db6fd179e55
sguha-work <sguha1988.life@gmail.com>
2019-09-23 17:13:13 +0530

    day 1 continues

--- Adv_Python_Training/Day1/io.py
+++ Adv_Python_Training/Day1/io.py
@@ -1,6 +1,11 @@
 print('==========================================')
-print("""Enter a value""")
-a=input()
-print('The given value is ',a)
-print('The type of given value is ',type(a))
+#print("""Enter a value""")
+#a=input()
+#print('The given value is ',a)
+#print('The type of given value is ',type(a))
+
+print('Enter an expression')
+exp = input()
+exp = exp
+print('The result ',eval(exp))
 print('==========================================')
