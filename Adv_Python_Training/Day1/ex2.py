95f6e81683c2baf96b8e88c98d0750ce23e89d75
sguha-work <sguha1988.life@gmail.com>
2019-09-23 16:50:48 +0530

    day 1 continues

---
+++ Adv_Python_Training/Day1/ex2.py
@@ -0,0 +1,18 @@
+print('==================================================')
+a=10
+b=10
+list45 = [10, 20, 30, 40, 50]
+if (a in list45):
+    print("""'A' is in the list""")
+else:
+    print("""'A' is not in the list""")
+if (b not in list45):
+    print("""'B' is not in the list""")
+else:
+    print("""'B' is in the list""")
+
+if(a is not b):
+    print("""'A' and 'B' is not same """)
+else:
+    print("""'A' and 'B' is same """)
+print('==================================================')
