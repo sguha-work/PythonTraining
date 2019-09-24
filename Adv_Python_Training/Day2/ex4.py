f1980a917d0f71a5342097033a58a714c9f85ef8
sguha-work <sguha1988.life@gmail.com>
2019-09-24 10:56:13 +0530

    day2 continues

--- Adv_Python_Training/Day2/ex4.py
+++ Adv_Python_Training/Day2/ex4.py
@@ -6,20 +6,29 @@ burgerPrice = 30
 pizzaPrice = 100
 soupPrice = 40
 priceList = [burgerPrice, pizzaPrice, soupPrice]
-print('Food Items')
-print(' 1. Burger')
-print(' 2. Pizza')
-print(' 3. Soup')
-print('Enter your choice of item:',end='')
+displayString ="""
+    Food Items
+        1. Burger
+        2. Pizza
+        3. Soup
+    Enter your choice of item:"""
+print(displayString,end='')
+
 userChoiceOfFood = int(input())-1
-print ('Enter quantity:',end='')
+
+print ('    Enter quantity:',end='')
 userChoosenQuantity = int(input())
+
 basicFoodPrice = priceList[userChoiceOfFood]*userChoosenQuantity
 calculatedCGST = (basicFoodPrice * 2.5)/100
 calculatedSGST = (basicFoodPrice * 2.5)/100
 actualFoodPrice = basicFoodPrice + calculatedCGST + calculatedSGST
-print(f'Total tax to be paid {calculatedCGST+calculatedSGST} rupees')
-print(f'Food price {basicFoodPrice} rupees')
-print(f'Total paybel ammount {actualFoodPrice} rupees')
-print('Thank you visit again') 
+
+displayString = f"""
+    Food price:             {basicFoodPrice} rupees
+    Total tax to be paid:   {calculatedCGST+calculatedSGST} rupees
+    Total paybel ammount:   {actualFoodPrice} rupees
+    Thank you visit again
+"""
+print(displayString)
 print('==============================================================')
