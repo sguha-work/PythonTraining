0db89890bec2ad289c8bc484cfe3aa11b324d95b
sguha-work <sguha1988.life@gmail.com>
2019-09-24 15:09:35 +0530

    day2 continues

---
+++ Adv_Python_Training/Day2/ex7.py
@@ -0,0 +1,7 @@
+print('==============================================================')
+food = 'spam'
+if food == 'spam':
+    print('Ummmmmm! My favourite')
+print('I feel like saying it 100 times....')
+print(100*(food+'!\n')) # if we remove \n then all will be printed in same line
+print('==============================================================')
