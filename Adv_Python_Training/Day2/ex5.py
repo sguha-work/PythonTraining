65afc5e24fd19b794084d4915f0e99082e7e4150
sguha-work <sguha1988.life@gmail.com>
2019-09-24 12:00:09 +0530

    day2 continues

---
+++ Adv_Python_Training/Day2/ex5.py
@@ -0,0 +1,15 @@
+## Dictionary
+print('==============================================================')
+name = 'Angshu'
+a = {
+    'name': name,
+    'age': 30,
+    'address': 'sodepur',
+    123: 'ABCD'
+}
+print(a)
+print(type(a))
+print(a['name'])
+print(a[123])
+#print(a.name) # this will cause error
+print('==============================================================')
